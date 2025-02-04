a1bfbf7024d29785975cdbf09ff08440b0e647c5
lacelyn5 <maximkerno@gmail.com>
2025-02-04 13:51:17 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 define('DATA_HOST', 'db'); 
 define('DATA_NAME', 'event_management');
-define('DATA_USER', 'Lacelyn'); 
-define('DATA_PASS', 'maximkerno'); 
+define('DATA_USER', 'root'); 
+define('DATA_PASS', 'rootpassword'); 
 spl_autoload_register(function ($class) {
     include __DIR__ . '/classes/' . $class . '.php';
 });
